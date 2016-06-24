1d48696a5714282553a04c3ed745c46b1c6ab7c2
Michael Mclawhorn <mclawhor@yahoo-inc.com>
2016-06-24 15:36:56 -0500

    Added debugging and tracing.

--- travis/deploy.bash
+++ travis/deploy.bash
@@ -3,9 +3,22 @@ PUBLISH_WHITELIST="master foo"
 
 # We only publish on commits to whitelisted branches, not PRs against them or tagging events
 # This isn't a pull request build
-[[ "${TRAVIS_PULL_REQUEST}" == "false" ]] \
-  && [[ "${TRAVIS_TAG}" == "" ]] \
-  && [[ ${PUBLISH_WHITELIST} =~ ${TRAVIS_BRANCH} ]] \
-  && travis/ci-tag && mvn versions:set -DnewVersion=$(git describe) \
+if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
+    echo "Not publishing, this is PR: ${TRAVIS_PULL_REQUEST}"
+    exit 0
+fi
+
+if [[ "${TRAVIS_TAG}" == "" ]]; then
+    echo "Not publishing, this is a tag:  ${TRAVIS_TAG}"
+    exit 0
+fi
+
+
+if [[ ${PUBLISH_WHITELIST} =~ ${TRAVIS_BRANCH} ]]; then
+    echo "Not publishing, ${TRAVIS_BRANCH} is not in whitelist: ${PUBLISH_WHITELIST}"
+    exit 0
+fi
+
+travis/ci-tag && mvn versions:set -DnewVersion=$(git describe) \
   && mvn deploy --settings travis/bintray-settings.xml
   && mvn versions:revert ;
