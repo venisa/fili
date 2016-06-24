6a94463215f665d1fbbfb8636b5a766961bbca33
Michael Mclawhorn <mclawhor@yahoo-inc.com>
2016-06-24 15:18:34 -0500

    Build everything, publish from whitelist, auto versioning.

--- travis/deploy.bash
+++ travis/deploy.bash
@@ -1,6 +1,11 @@
 #/bin/bash
-test "${TRAVIS_PULL_REQUEST}" == "false" \
-       && test "${TRAVIS_TAG}" != "" \
-       && mvn versions:set -DnewVersion=${TRAVIS_TAG} \
-       && mvn deploy --settings travis/bintray-settings.xml ;
+PUBLISH_WHITELIST="master foo"
 
+# We only publish on commits to whitelisted branches, not PRs against them or tagging events
+# This isn't a pull request build
+[[ "${TRAVIS_PULL_REQUEST}" == "false" ]] \
+  && [[ "${TRAVIS_TAG}" == "" ]] \
+  && [[ ${PUBLISH_WHITELIST} =~ ${TRAVIS_BRANCH} ]] \
+  && travis/ci-tag && mvn versions:set -DnewVersion=$(git describe) \
+  && mvn deploy --settings travis/bintray-settings.xml
+  && mvn versions:revert ;
--- travis/upversion.py
+++ travis/upversion.py
@@ -2,6 +2,6 @@
 
 import os
 
-split_tag = os.environ['LAST_TAG'].split(".")
+split_tag = (os.environ['LAST_TAG'].split('-')[0]).split(".")
 split_tag[-1] = str(int(split_tag[-1]) + 1)
 print ".".join(split_tag)
