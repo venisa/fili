b3907049532ae08b6561d8bc2960451ceda3955e
Michael Mclawhorn <mclawhor@yahoo-inc.com>
2016-08-30 18:36:33 -0500

    Turning checkstyle on.

--- fili-core/src/main/java/com/yahoo/bard/webservice/web/endpoints/DataServlet.java
+++ fili-core/src/main/java/com/yahoo/bard/webservice/web/endpoints/DataServlet.java
@@ -29,7 +29,6 @@ import com.yahoo.bard.webservice.jobs.JobPayloadBuilder;
 import com.yahoo.bard.webservice.jobs.JobRow;
 import com.yahoo.bard.webservice.jobs.JobRowBuilder;
 import com.yahoo.bard.webservice.jobs.MetadataHttpResponseChannel;
-import com.yahoo.bard.webservice.jobs.PreResponseStore;
 import com.yahoo.bard.webservice.logging.RequestLog;
 import com.yahoo.bard.webservice.logging.blocks.BardQueryInfo;
 import com.yahoo.bard.webservice.logging.blocks.DataRequest;
@@ -134,7 +133,8 @@ public class DataServlet extends CORSPreflightServlet implements BardConfigResou
      * @param jobRowBuilder  The JobRows factory
      * @param asynchronousWorkflowsBuilder  The factory for building the asynchronous workflow
      * @param preResponseStoredNotifications  The broadcast channel responsible for notifying other Bard prcesses
-     * that a query has been completed and its results stored in the {@link PreResponseStore}
+     * that a query has been completed and its results stored in the
+     * {@link com.yahoo.bard.webservice.jobs.PreResponseStore}
      */
     @Inject
     public DataServlet(
@@ -452,7 +452,7 @@ public class DataServlet extends CORSPreflightServlet implements BardConfigResou
         // the queryResultsEmitter emitting a PreResponse (i.e. the system hasn't finished processing the query), then
         // instead of emitting a PreResponse, we emit a JobRow, which triggers all of the asynchronous processing.
         ConnectableObservable<Either<PreResponse, JobRow>> payloadEmitter = queryResultsEmitter
-                .map(Either::<PreResponse, JobRow>left)
+                .map(Either::<PreResponse, JobRow> left)
                 .timeout(
                         asyncAfter,
                         TimeUnit.MILLISECONDS,
